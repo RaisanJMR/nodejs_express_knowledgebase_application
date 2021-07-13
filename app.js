d4958cc0e065d374fa8fc57ba019a8d3a13f62f0
RaisanJMR <raisanjmr@protonmail.com>
2021-07-13 07:54:33 +0530

    second commit

--- app.js
+++ app.js
@@ -4,17 +4,40 @@ const path = require('path');
 const app = express();
 
 // load view engine
-app.set('views',path.join(__dirname, 'views'));
-app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 // home routes
 app.get('/', (req, res) => {
-  res.render('index',{heading: 'article', title: 'node_app'});
+  let articles = [
+    {
+      id: 1,
+      title: 'Article one',
+      author: 'Author one',
+      aticle: 'this is article one',
+    },
+    {
+      id: 2,
+      title: 'Article two',
+      author: 'Author two',
+      aticle: 'this is article two',
+    },
+    {
+      id: 3,
+      title: 'Article three',
+      author: 'Author three',
+      aticle: 'this is article three',
+    },
+  ];
+  res.render('index', { heading: 'article', title: 'node_app', articles: articles});
 });
 
 // add route
-app.get('/articles/add',(req,res)=>{
-  res.render('add',{heading: 'Add Article', title: 'node_app | ADD'});
-})
+app.get('/articles/add', (req, res) => {
+  res.render('add_article ', {
+    heading: 'Add Article',
+    title: 'node_app | ADD',
+  });
+});
 
 // port
 const PORT = 3000;
