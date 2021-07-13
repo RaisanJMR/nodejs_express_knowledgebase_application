d8581063a562d5925e1c005e705092a2875bfc10
RaisanJMR <raisanjmr@protonmail.com>
2021-07-13 16:37:33 +0530

    completed CRUD operation

--- app.js
+++ app.js
@@ -22,11 +22,13 @@ db.on('error', function (err) {
 // init app
 const app = express();
 
-
 // MIDDLEWARE
 // bodyparser allows us to take incoming post request bodies
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// set public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Bring in database models
 let Article = require('./models/article');
@@ -42,11 +44,21 @@ app.get('/', (req, res) => {
       console.log(err);
     } else {
       res.render('index', {
-        heading: 'article',
-        title: 'node_app',
+        heading: 'Articles',
+        title: 'knowledgeBase | HOME',
         articles: articles,
       });
     }
+  });
+});
+
+// get single route
+app.get('/article/:id', (req, res) => {
+  Article.findById(req.params.id, (err, article) => {
+    res.render('article', {
+      article: article,
+      title: `knowledgeBase | ${article.title}`,
+    });
   });
 });
 
@@ -54,7 +66,7 @@ app.get('/', (req, res) => {
 app.get('/articles/add', (req, res) => {
   res.render('add_article', {
     heading: 'Add Article',
-    title: 'node_app | ADD',
+    title: 'knowledgeBase | ADD ARTICLE',
   });
 });
 
@@ -65,15 +77,63 @@ app.post('/articles/add', (req, res) => {
   article.author = req.body.author;
   article.body = req.body.body;
 
-  article.save((err)=>{
-    if(err) {
-      console.log(err)
-    }else {
-      res.redirect('/')
+  article.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
     }
-  })
-  
+  });
 });
+
+
+
+// load edit form
+app.get('/article/edit/:id', (req, res) => {
+  Article.findById(req.params.id, (err, article) => {
+    res.render('edit_article', {
+      article: article,
+      heading: 'Edit article',
+      title: `knowledgeBase | edit ${article.title}`,
+    });
+  });
+});
+
+
+
+// edit submit POST route
+app.post('/articles/edit/:id', (req, res) => {
+  let article = {};
+  let query = { _id: req.params.id };
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  Article.updateOne(query, article, function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+
+
+// DELETE request
+app.delete('/article/:id', (req, res) => {
+  let query = { _id: req.params.id };
+  Article.deleteOne(query, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send('sucess');
+  });
+});
+
+
+
 // port
 const PORT = 3000;
 app.listen(3000, () => {
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 # Nodejs Express application
+
+### Tech stack used
+
+ - nodejs
+ - express
+ - mongodb
+ - pug (templating engine)
+ - Bower (front end package manager)
